ce8af2274419327067f1daded177982e5025fd6b
deepesh-thoduvayil <49249547+deepesh-thoduvayil@users.noreply.github.com>
2023-07-06 00:40:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -63,13 +63,13 @@ variable "pod_network_cidr_block" {
 variable "master_instance_type" {
   type        = string
   description = "EC2 instance type for the master node (must have at least 2 CPUs and 2 GB RAM)."
-  default     = "t2.medium"
+  default     = "t3.medium"
 }
 
 variable "worker_instance_type" {
   type        = string
   description = "EC2 instance type for the worker nodes."
-  default     = "t2.small"
+  default     = "t3.medium"
 }
 
 variable "num_workers" {
